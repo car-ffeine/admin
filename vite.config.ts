4d7703ae54a3fbbc9349fb77c413cc8801261ed3
Dain Lee <feb.dain@gmail.com>
2023-08-12 18:51:09 +0900

    chore: `vite-plugin-svgr` 설치 및 assets 절대 경로 추가
    [#13]

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   base: './',
 });
