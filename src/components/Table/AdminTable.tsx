c6452a96b7ae1bbdacbb5aff4accbd246a1b7482
Dain Lee <feb.dain@gmail.com>
2023-08-14 23:58:47 +0900

    feat: 상세 정보, 정보 수정 버튼 구현
    [#23]

--- src/components/Table/AdminTable.tsx
+++ src/components/Table/AdminTable.tsx
@@ -1,5 +1,7 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
+import { PencilSquareIcon } from '@heroicons/react/24/outline';
 import { Box } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -28,9 +30,21 @@ function AdminTable({ title }: TableProps) {
 
   return (
     <Box sx={{ margin: '32px' }}>
-      <Title>{title}</Title>
+      <TitleContainer>
+        <Title tabIndex={0}>{title}</Title>
+        <ButtonContainer>
+          <Button type="button" aria-label="수정">
+            <span>수정</span>
+            <PencilSquareIcon width={24} />
+          </Button>
+          <Button type="button" aria-label="상세보기">
+            <span>상세보기</span>
+            <EllipsisVerticalIcon width={24} />
+          </Button>
+        </ButtonContainer>
+      </TitleContainer>
       <TableContainer component={Paper} css={boxShadowCss}>
-        <Table sx={{ minWidth: 500, overflowX: 'auto' }} aria-label="정보 표">
+        <Table tabIndex={0} sx={{ minWidth: 500, overflowX: 'auto' }} aria-label={`${title} 정보`}>
           <AdminTableHead categoryList={STATION_DETAILS_CATEGORY_LIST} />
           <AdminTableBody elements={stationSummaryList} />
         </Table>
@@ -40,11 +54,45 @@ function AdminTable({ title }: TableProps) {
   );
 }
 
-const Title = styled.h1`
-  margin-bottom: 24px;
-  padding-top: 8px;
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 28px;
+  margin: 16px 0 24px;
+`;
+
+const Title = styled.h2`
+  margin: 0;
+  margin-right: 40px;
   font-size: 19px;
   color: #333;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  column-gap: 8px;
+  margin-right: 12px;
+  &:last-child {
+    margin-right: 0;
+  }
+
+  background: transparent;
+  border: 1.4px solid #515151;
+  border-radius: 8px;
+  padding: 2px 8px;
+  cursor: pointer;
+
+  &:hover {
+    background: #fafafa;
+    border: 1.4px solid #333;
+    transform: translateY(-2px);
+  }
 `;
 
 const boxShadowCss = css`
