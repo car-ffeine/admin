982510444df34e3c07a0fef71425bcbc29494e90
Dain Lee <feb.dain@gmail.com>
2023-08-15 17:28:11 +0900

    refactor: production 모드와 development 모드일 때 BASE_URL 다르게 변경
    [#28]

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -3,4 +3,5 @@ export const LOGIN_BASE_URL =
     ? 'https://api.carffe.in/api'
     : 'https://dain.carffe.in/api';
 
-export const BASE_URL = 'https://api.carffe.in/api/admin';
+export const BASE_URL =
+  import.meta.env.MODE === 'production' ? `${LOGIN_BASE_URL}/admin` : `${LOGIN_BASE_URL}/admin`;
